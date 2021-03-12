8e386c13d694e320b34f0b8457fba9d31f271bba
Samuel Hazeldine <samhazeldine@hotmail.co.uk>
2021-03-12 16:54:07 +0000

    Working on the carousel

---
+++ src/components/Carousel.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { fade } from "svelte/transition";
+
+    const images = [
+        "http://placekitten.com/200/200",
+        "http://placekitten.com/202/202",
+        "http://placekitten.com/201/201",
+    ];
+
+    let imageIndex = 0;
+
+    function nextImage() {
+        imageIndex = (imageIndex + 1) % images.length;
+    }
+
+</script>
+
+
+<div id="cascade">
+    {#each [images[imageIndex]] as image (imageIndex)}
+        <img id="cascade__img"  src={image} alt="alt" transition:fade="{{duration:1000}}"/>
+    {/each}
+</div>
+
+<button on:click={nextImage}> Next Image </button>
+
+<p> I want this below</p>
+
+<style>
+    #cascade {
+        position:relative;
+    }
+    #cascade__img {
+        position:absolute;
+    }
+    button{
+        position:relative;
+    }
+</style>
